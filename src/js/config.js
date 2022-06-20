bfc481ff6c81f52ac8a77bf45cf03c58fd53afa8
Prateek Rath <prateekrath28@gmail.com>
2022-06-20 21:56:48 +0530

    Update API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 11;
-export const KEY = '3c933c30-d0fc-4ead-b63a-fcd022ad1be5';
+export const KEY = '7e53a2c4-c019-4079-b244-11c1ee7cf3b8';
 export const MODAL_CLOSE_SEC = 2.5;
