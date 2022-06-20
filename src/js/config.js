307ce6ca191cb2674a0c87255b3828bba4ac0d89
Prateek Rath <prateekrath28@gmail.com>
2022-06-20 19:41:08 +0530

    added configs

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 11;
+export const KEY = '3c933c30-d0fc-4ead-b63a-fcd022ad1be5';
+export const MODAL_CLOSE_SEC = 2.5;
